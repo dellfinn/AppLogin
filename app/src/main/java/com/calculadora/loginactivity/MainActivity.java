e56813f4f85573a3ba6c1a9a59de5702abe4a47e
Italo <paradoxonitalo@gmail.com>
2021-06-28 22:09:57 -0300

    App Login Java

---
+++ app/src/main/java/com/calculadora/loginactivity/MainActivity.java
@@ -0,0 +1,48 @@
+package com.calculadora.loginactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText mEditEmail;
+    private EditText mEditPassword;
+    private Button mBtnEnter;
+    private TextView mTxtAccount;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mEditEmail = findViewById(R.id.edit_email);
+        mEditPassword = findViewById(R.id.edit_password);
+        mBtnEnter = findViewById(R.id.btn_enter);
+        mTxtAccount = findViewById(R.id.txt_account);
+
+        mBtnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               String email = mEditEmail.getText().toString();
+                String password = mEditPassword.getText().toString();
+                Log.i("Teste",email);
+                Log.i("Teste",password);
+
+            }
+        });
+        mTxtAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
